d25cd9e04193808fe95faa631b27c23cf6ca08d5
salem <salem.bajjali@gmail.com>
2023-09-06 11:54:57 -0500

    update hgvs_validation.py

--- src/hgvs/hgvs_validation.py
+++ src/hgvs/hgvs_validation.py
@@ -5,64 +5,67 @@ import hgvs.dataproviders.uta
 import hgvs.validator
 from hgvs.exceptions import HGVSError
 
-def validate_hgvs_variants(hp, vr, hgvs_list):
-    val_output = []
+class HGVSValidator:
+    def __init__(self,input_file,output_file,example_column):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.example_column = example_column
 
-    for hgvs in hgvs_list:
+        self.hp = hgvs.parser.Parser()
+        self.hdp = hgvs.dataproviders.uta.connect()
+        self.vr = hgvs.validator.Validator(hdp=self.hdp)
+
+    def _validate_hgvs_variants(self,hgvsExpressions):
+        validated_results = []
+
+        for hgvs in hgvsExpressions:
+            try:
+                if isinstance(hgvs, str):
+                    parsed_variant = self.hp.parse_hgvs_variant(hgvs)
+                    self.vr.validate(parsed_variant)
+                    validated_results.append(True)
+                else:
+                    validated_results.append(False)
+            except HGVSError as e:
+                validated_results.append(e)
+                
+        return validated_results
+    
+    def load_data(self):
         try:
-            if isinstance(hgvs, str):
-                parsed_variant = hp.parse_hgvs_variant(hgvs)
-                vr.validate(parsed_variant)
-                val_output.append(True)
-            else:
-                val_output.append(False)  
-        except HGVSError as e:
-            val_output.append(e)
-            
-    return val_output
+            input_data = pd.read_excel(self.input_file)
+        except Exception:
+            try:
+                input_data = pd.read_csv(self.input_file, sep=",")
+            except Exception:
+                print("Error: Unsupported file format. Please provide an Excel or CSV file.")
+        
+        return input_data 
+    
+    def validation_results(self,input_data):
+        
+        input_data[self.example_column] = input_data[self.example_column].str.strip()
+        hgvsExamples = input_data[self.example_column]
+        validation_results = self._validate_hgvs_variants(hgvsExamples)
 
-def main():
-    parser = argparse.ArgumentParser(description="Validate HGVS expressions from an Excel, CSV, or tab-delimited file.")
+        hgvs_results = {
+            'HGVS': hgvsExamples,
+            'Validator': validation_results
+        }
+
+        result_df = pd.DataFrame(hgvs_results)
+        result_df.to_csv(self.output_file, index=False)
+
+        print("Script execution completed.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Validate HGVS expressions from an Excel or CSV file.")
     parser.add_argument("input_file", help="Path to the input file")
     parser.add_argument("output_file", help="Path to the output CSV file")
     parser.add_argument("example_column", help="Name of the column containing HGVS examples")
 
     args = parser.parse_args()
 
-    hp = hgvs.parser.Parser()
-    hdp = hgvs.dataproviders.uta.connect()
-    vr = hgvs.validator.Validator(hdp=hdp)
-    
-# TODO: need to test that the files open up correctly
-    try:
-        input_data = pd.read_excel(args.input_file)
-    except Exception:
-        try:
-            input_data = pd.read_csv(args.input_file, sep=",")
-        except Exception:
-            try:
-                input_data = pd.read_csv(args.input_file, sep="\t")
-            except Exception:
-                print("Error: Unsupported file format. Please provide an Excel, CSV, or tab-delimited file.")
-                return
-
-    input_data[args.example_column] = input_data[args.example_column].str.strip()
-
-    data = pd.DataFrame(input_data)
-    hgvs_expression = data[args.example_column]
-
-    original_error_messages = validate_hgvs_variants(hp,vr,hgvs_expression)
-
-    hgvs_results = {
-        'HGVS': data[args.example_column],
-        'biocommons_validator': original_error_messages
-    }
-
-    result = pd.DataFrame(hgvs_results)
-
-    result.to_csv(args.output_file, index=False)
-
-    print("Script execution completed.")
-
-if __name__ == "__main__":
-    main()
+    hgvs_validator = HGVSValidator(args.input_file,args.output_file,args.example_column)
+    data = hgvs_validator.load_data()
+    hgvs_validator.validation_results(data)
